8768898a1bee6b7ddc28cf25fcf1efc957101af2
OTLC <onthelinecoders@gmail.com>
2015-12-12 15:52:40 -0500

    completed creation of create, show posts as well as updating root home page to default to displaying all posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,31 @@
 class PostsController < ApplicationController
   # This is called a controller action
+  # This is the fist action. we will create
+  # additional actions to create, update, delete.
   def index
+  	@posts = Post.all.order("created_at DESC")
+  end
+
+  def new
+  	@post = Post.new
+  end
+
+  def create
+  	@post = Post.new(post_params)
+  	if @post.save
+  		redirect_to @post
+  	else
+  		render 'new'
+  	end
+  end
+
+  def show
+  	@post = Post.find(params[:id])
+  end
+ 
+  private
+
+  def post_params
+  	params.require(:post).permit(:title, :body)
   end
 end
